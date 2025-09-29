2a9db2b2a23059dcfd885fabde54c8fa09e11097
Alexandr Pligin <rogchik@gmail.com>
2025-09-29 09:32:45 +0500

    corrections 4

--- src/download-resources.js
+++ src/download-resources.js
@@ -3,36 +3,36 @@ import { promises as fs } from 'fs'
 import Listr from 'listr'
 import path from 'path'
 
-const downloadResources = async (resources, resourcesDirName, { strict = true, log = console } = {}) => {
+const downloadResources = (resources, resourcesDirName, { strict = true, log = console } = {}) => {
   const tasks = new Listr(
     resources.map(({ resourceUrl, resourcePath }) => ({
       title: `Downloading: ${resourceUrl}`,
-      task: async () => {
-        try {
-          const res = await axios.get(resourceUrl, { responseType: 'arraybuffer' })
-          if (res.status !== 200) {
-            throw new Error(`Unexpected status code ${res.status} for ${resourceUrl}`)
-          }
-          await fs.writeFile(resourcePath, res.data)
-        }
-        catch (err) {
-          if (strict) {
-            throw new Error(`Failed to download resource ${resourceUrl}: ${err.message}`)
-          }
-          else {
-            log.warn(`Failed to download resource ${resourceUrl}: ${err.message}`)
-          }
-        }
-      },
+      task: () =>
+        axios.get(resourceUrl, { responseType: 'arraybuffer' })
+          .then((res) => {
+            if (res.status !== 200) {
+              throw new Error(`Unexpected status code ${res.status} for ${resourceUrl}`)
+            }
+            return fs.writeFile(resourcePath, res.data)
+          })
+          .catch((err) => {
+            if (strict) {
+              throw new Error(`Failed to download resource ${resourceUrl}: ${err.message}`)
+            }
+            else {
+              log.warn(`Failed to download resource ${resourceUrl}: ${err.message}`)
+            }
+          }),
     })),
     { concurrent: true },
   )
 
-  await tasks.run()
-
-  resources.forEach(({ el, attr, resourceFilename }) => {
-    el.attribs[attr] = path.join(resourcesDirName, resourceFilename)
-  })
+  return tasks.run()
+    .then(() => {
+      resources.forEach(({ el, attr, resourceFilename }) => {
+        el.attribs[attr] = path.join(resourcesDirName, resourceFilename)
+      })
+    })
 }
 
 export default downloadResources
