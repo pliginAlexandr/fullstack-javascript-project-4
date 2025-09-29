0495130f02b79b198501ad94001440a87e1762c0
Alexandr Pligin <rogchik@gmail.com>
2025-09-29 09:36:13 +0500

    corrections 5

--- src/page-loader.js
+++ src/page-loader.js
@@ -5,6 +5,27 @@ import debug from 'debug'
 import parseResources from './parse-resources.js'
 import downloadResources from './download-resources.js'
 import { makeFilename, makeDirName } from './utils.js'
+import axiosDebug from 'axios-debug-log'
+
+if (process.env.NODE_ENV === 'test') {
+  axiosDebug({
+    request: (debug, config) => {
+      debug('Request', config.method?.toUpperCase(), config.url)
+    },
+    response: (debug, response) => {
+      debug('Response', response.status, response.config.url)
+    },
+    error: (debug, error) => {
+      if (error.response) {
+        debug('Error', error.response.status, error.config.url)
+      }
+      else {
+        debug('Network Error', error.message)
+      }
+    },
+  })
+  axiosDebug.addLogger(axios)
+}
 
 const log = debug('page-loader')
 
