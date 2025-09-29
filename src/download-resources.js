165dd4f76eef398861b4f069a527b4842544fd68
Alexandr Pligin <rogchik@gmail.com>
2025-09-29 09:19:08 +0500

    corrections 3

--- src/download-resources.js
+++ src/download-resources.js
@@ -7,21 +7,19 @@ const downloadResources = (resources, resourcesDirName) => {
   const tasks = new Listr(
     resources.map(({ resourceUrl, resourcePath, el, attr, resourceFilename }) => ({
       title: `Downloading: ${resourceUrl}`,
-      task: () =>
-        axios
-          .get(resourceUrl, { responseType: 'arraybuffer' })
-          .then((res) => {
-            if (res.status !== 200) {
-              throw new Error(`Unexpected status code ${res.status} for ${resourceUrl}`)
-            }
-            return fs.writeFile(resourcePath, res.data)
-          })
-          .then(() => {
-            el.attribs[attr] = path.join(resourcesDirName, resourceFilename)
-          })
-          .catch((err) => {
-            throw new Error(`Failed to download resource ${resourceUrl}: ${err.message}`)
-          }),
+      task: async () => {
+        try {
+          const res = await axios.get(resourceUrl, { responseType: 'arraybuffer' })
+          if (res.status !== 200) {
+            throw new Error(`Unexpected status code ${res.status} for ${resourceUrl}`)
+          }
+          await fs.writeFile(resourcePath, res.data)
+          el.attribs[attr] = path.join(resourcesDirName, resourceFilename)
+        }
+        catch (err) {
+          throw new Error(`Failed to download resource ${resourceUrl}: ${err.message}`)
+        }
+      },
     })),
     { concurrent: true },
   )
